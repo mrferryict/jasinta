db947177925b4e674c5b6981379416df8b38d6de
mrferryict <mrferryict@gmail.com>
2025-02-06 22:01:16 +0700

    update 06012025

--- app/Language/id/App.php
+++ app/Language/id/App.php
@@ -1,5 +1,10 @@
 <?php
 return [
    'pleaseLogin' => 'Silakan Login',
+   'login' => 'Login',
+   'forgotPassword' => 'Lupa password',
+   'newRegister' => 'Pendaftaran Baru khusus Mahasiswa',
+   'email' => 'Surel',
+   'password' => 'Kata Sandi',
    'welcomeToOurSystem' => 'Welcome to our system!',
 ];
